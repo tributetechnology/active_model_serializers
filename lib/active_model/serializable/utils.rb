6c5444bd840ec57792f3c8bf1d941b0a0d316c5d
Christopher Ritsen <chris.ritsen@gmail.com>
2022-05-06 11:44:01 -0400

    Re-apply patch

--- lib/active_model/serializable/utils.rb
+++ lib/active_model/serializable/utils.rb
@@ -5,8 +5,22 @@ module ActiveModel
 
       def _const_get(const)
         begin
-          method = RUBY_VERSION >= '2.0' ? :const_get : :qualified_const_get
-          Object.send method, const
+          method = :const_get
+
+          # Cliff C-
+          # Upgrading from Ruby 2.6.5 to Ruby 2.6.6 seemed to break the
+          # ability to const_get an entire constant consistently. Chaining the
+          # fetch of each individual constant seems to work. As V1 will be
+          # replaced by V2 in the future we will use this patch temporarily.
+          # (A future Rails or Ruby update may also resolve this).
+
+          current_object = Object
+
+          const.split('::').each do |module_or_class|
+            current_object = current_object.send(method, module_or_class)
+          end
+
+          current_object
         rescue NameError
           const.safe_constantize
         end
